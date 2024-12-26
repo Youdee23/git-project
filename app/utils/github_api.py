71559bd57f72f03be08e02c628047389a15b2ec4
Uduak <uetimfon@gmail.com>
2024-12-26 08:39:17 +0100

    Files added:
    __init__.py
    github_api.py

---
+++ app/utils/github_api.py
@@ -0,0 +1,20 @@
+from typing import List
+import requests
+
+
+class GitHubApi:
+    """A utility for interacting with GitHub API"""
+    BASE_URL = "https://api.github.com"
+
+    def __init__(self, token: str):
+        """Class constructor"""
+        self.headers = {"Authorisation": f"token {token}"}
+
+    async def fetch_repositories(self, userUrl: str) -> List[dict]:
+        """Fetch repositories by username"""
+        username = userUrl.split('/')[-1]
+        url = f"{self.BASE_URL}/users/{username}/repos"
+        response = requests.get(url, headers=self.headers)
+        response.raise_for_status()
+        return response.json()
+    
